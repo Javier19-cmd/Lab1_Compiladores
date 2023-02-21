1597179150d79e3633a06f09f7fabb12975dc96d
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-02-21 10:47:07 -0600

    :v:

--- Errores.py
+++ Errores.py
@@ -15,28 +15,28 @@ def deteccion(regex):
     for i in range(len(regex)):
         if regex[i] == '+' or regex[i] == '*':
             if i == 0:
-                print("Error: La expresión regular no tiene letras.")
+                print("Error: la expresión tiene ++a.")
                 return False
             elif regex[i - 1] == '+' or regex[i - 1] == '*':
-                print("Error: La expresión regular no tiene letras.")
+                print("Error: La expresión tiene un +a.")
                 return False
         
         elif regex[i] == '.': # Verificando que en la expresión no existan cosas como ..a o .a.
             if i == 0:
-                print("Error: La expresión regular no tiene letras.")
+                print("Error: La expresión regular tiene .a")
                 return False
             elif regex[i - 1] == '.':
-                print("Error: La expresión regular no tiene letras.")
+                print("Error: La expresión tiene ..a")
                 return False
 
-    #Verificando que la expresión no tenga cosas como a|
+    # Verificando que la expresión no tenga cosas como a*+.
     for i in range(len(regex)):
-        if regex[i] == '|':
-            # El i debe ser un número impar para que sea una expresión regular válida.
-            if i % 2 == 0:
-                print("Error: La expresión regular no tiene letras.")
+        if regex[i] == '+':
+            if regex[i - 1] == '*':
+                print("Error: La expresión regular tiene un + a la par de un *")
                 return False
-            elif regex[i - 1] == '|':
-                print("Error: La expresión regular no tiene letras.")
+        elif regex[i] == '*':
+            if regex[i - 1] == '+':
+                print("Error: La expresión regular tiene un * a la par de un +.")
                 return False
     return True
